8c2e63ff4d6f9edc34a108ea08792d34f64bc3cf
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-06-07 06:28:48 -0400

    Add test script

---
+++ scripts/test.py
@@ -0,0 +1,9 @@
+import instrument
+from time import sleep
+
+melody = [('C4', 500), ('C4', 500), ('G4', 500), ('G4', 500), ('A4', 500), ('A4', 500), ('G4', 1000), ('F4', 500), ('F4', 500), ('E4', 500), ('E4', 500), ('D4', 500), ('D4', 500), ('C4', 1000)]
+
+for note, duration in melody:
+    stop = instrument.play(note = note, instrument="198_Glassheaven")
+    sleep(duration / 1000)
+    stop()
