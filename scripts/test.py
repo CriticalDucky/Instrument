6fe0d5d0061173c0f786633deacfef4f96d58e1d
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-06-09 17:13:13 -0400

    Change instrument

--- scripts/test.py
+++ scripts/test.py
@@ -4,6 +4,6 @@ from time import sleep
 melody = [('C4', 500), ('C4', 500), ('G4', 500), ('G4', 500), ('A4', 500), ('A4', 500), ('G4', 1000), ('F4', 500), ('F4', 500), ('E4', 500), ('E4', 500), ('D4', 500), ('D4', 500), ('C4', 1000)]
 
 for note, duration in melody:
-    stop = instrument.play(note = note, instrument="Candy Bee.sf2")
+    stop = instrument.play(note = note, instrument="Analog Saw.sf2")
     sleep(duration / 1000)
     stop()
