fe58546c5edcf1f8d13b4483c3b3a0e93c5c15a4
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-06-12 16:35:17 -0400

    Add debug test

---
+++ scripts/note_mappings.py
@@ -0,0 +1,14 @@
+'''
+Module for mapping ultrasonic sensor readings to notes
+'''
+
+octave_notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+num_octaves = 7
+
+# The note depends on the sensor number (1-12), and the octave depends on the distance (cm)
+# The distance is mapped to an octave, and the sensor number is mapped to a note in that octave
+# Octaves are separated by 4 cm.
+def get_note_name(sensor_number, distance):
+    octave = int(distance / 4)
+    note = octave_notes[(sensor_number - 1)]
+    return note + str(octave)
