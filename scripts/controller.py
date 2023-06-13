768a76548fe6b20f19c078d4b970e4e8291830e4
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-06-13 18:53:48 -0400

    Add priming

--- scripts/controller.py
+++ scripts/controller.py
@@ -10,7 +10,7 @@ BURST_INSTRUMENTS = [ # Instruments that we do not need to stop playing when we 
 NUM_SENSORS = 1
 
 active_notes = {
-    # "C4": (stop_func, is_prime)
+    # "C4": (stop_func, is_primed)
 }
 
 while True:
@@ -18,18 +18,24 @@ while True:
         distance = get_distance(sensor_number)
         note = get_note_name(sensor_number, distance)
         selected_instrument = get_selected_instrument()
+        stop_func, is_primed = active_notes.get(note, (None, False))
 
         if note is None and note not in BURST_INSTRUMENTS:
-            stop = active_notes.get(note)
-
-            if stop:
-                stop()
+            if stop_func:
+                stop_func()
                 del active_notes[note]
             
             continue
+        else:
+            for active_note in active_notes:
+                if not active_note.startswith(note[1]): # We only want the notes this sensor is responsible for
+                    continue
 
-        if note not in active_notes or selected_instrument in BURST_INSTRUMENTS:
-            print("Playing note", note)
-            active_notes[note] = play(selected_instrument, note)
+                if active_note != note:
+                    active_notes[active_note] = (stop_func, False)
+
+            if note not in active_notes or selected_instrument in BURST_INSTRUMENTS and not is_primed:
+                print("Playing note", note)
+                active_notes[note] = (play(selected_instrument, note), True)
 
     sleep(UPDATE_INTERVAL)
