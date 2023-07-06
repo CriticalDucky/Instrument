70ff653c9457be2387f499cd36335e2fd7b22d6f
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-07-05 23:07:55 -0400

    Adjust spacing

--- scripts/note_mappings.py
+++ scripts/note_mappings.py
@@ -2,9 +2,10 @@
 Module for mapping tof sensor readings to notes
 '''
 BASELINE_DISTANCE = 4  # cm; distance from sensor to first octave
-OCTAVE_SPAN_SIZE = 1  # cm; size of activation area of each octave
-OCTAVE_SPACING = 2  # cm; distance between octaves
+OCTAVE_SPAN_SIZE = 1.5  # cm; size of activation area of each octave
+OCTAVE_SPACING = 1.5  # cm; distance between octaves
 FINGER_WIDTH_HALF = 1.5/2  # cm; width of finger divided by 2
+DIAMETER = BASELINE_DISTANCE * 2 + 7 * (OCTAVE_SPAN_SIZE + OCTAVE_SPACING)
 
 RESPONSE_TOO_CLOSE = "rTC"
 RESPONSE_TOO_FAR = "rTF"
