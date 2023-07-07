91c5ac54c84869b460a2a11d6a3eb9db2c70bc76
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-07-07 13:54:00 -0400

    Change test values

--- scripts/test.py
+++ scripts/test.py
@@ -8,7 +8,7 @@ distance_history = np.zeros(history_length)  # Initialize an array to store the 
 plt.figure()
 ax = plt.axes()
 ax.set_xlim([-history_length, 0])
-ax.set_ylim([0, 200])  # Adjust the y-axis limits as needed
+ax.set_ylim([0, 50])  # Adjust the y-axis limits as needed
 
 bars = ax.bar(np.arange(-history_length, 0), distance_history)  # Create an initial set of empty bars
 plt.ion()
@@ -16,7 +16,7 @@ plt.show()
 
 while True:
     # Read the distance measurement from your VL53L0X sensor
-    distance = np.random.randint(0, 200)
+    distance = np.random.randint(0, 50)
 
     # Shift the distance history array to the left and append the new measurement
     distance_history = np.roll(distance_history, -1)
