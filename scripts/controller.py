8298b3bbb304a37f8bfdb206bd64720e4d6b34a3
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-07-13 20:30:20 -0400

    Improve performance

--- scripts/controller.py
+++ scripts/controller.py
@@ -3,7 +3,7 @@ from time import sleep
 from tof import get_distance, close_sensors
 from instrument import play, get_selected_instrument
 
-UPDATE_HZ = 300
+UPDATE_HZ = 100
 BURST_INSTRUMENTS = [  # Instruments that we do not need to stop playing when we change notes
     "Acid SQ Neutral.sf2",
     "Piano.sf2"
@@ -20,60 +20,55 @@ active_notes = {
 def noteBelongsToSensor(note: str, sensor_number):
     return note.startswith(octave_notes[sensor_number - 1])
 
-try:
-    while True:
-        for sensor_number in range(1, NUM_SENSORS + 1):
-            distance = get_distance(sensor_number)
-            note = get_note_name(sensor_number, distance)
-            selected_instrument = get_selected_instrument()
+while True:
+    for sensor_number in range(1, NUM_SENSORS + 1):
+        distance = get_distance(sensor_number)
+        note = get_note_name(sensor_number, distance)
+        selected_instrument = get_selected_instrument()
 
-            if note in [
-                note_mappings.RESPONSE_NOT_IN_PATH,
-                note_mappings.RESPONSE_TOO_CLOSE,
-                note_mappings.RESPONSE_TOO_FAR
-            ]:
+        if note in [
+            note_mappings.RESPONSE_NOT_IN_PATH,
+            note_mappings.RESPONSE_TOO_CLOSE,
+            note_mappings.RESPONSE_TOO_FAR
+        ]:
 
-                for tup in active_notes.copy().items():
-                    active_note = tup[0]
+            for tup in active_notes.copy().items():
+                active_note = tup[0]
 
-                    # We only want the notes this sensor is responsible for
-                    if not noteBelongsToSensor(active_note, sensor_number):
-                        continue
+                # We only want the notes this sensor is responsible for
+                if not noteBelongsToSensor(active_note, sensor_number):
+                    continue
 
-                    if selected_instrument not in BURST_INSTRUMENTS and active_notes[active_note][0]:
-                        active_notes[active_note][0]()
-                        del active_notes[active_note]
+                if selected_instrument not in BURST_INSTRUMENTS and active_notes[active_note][0]:
+                    active_notes[active_note][0]()
+                    del active_notes[active_note]
+                else:
+                    active_notes[active_note] = (None, False)
+        elif note == note_mappings.RESPONSE_IN_SPACING:
+            continue
+        else:
+            for tup in active_notes.copy().items():
+                active_note = tup[0]
+
+                # We only want the notes this sensor is responsible for
+                if not noteBelongsToSensor(active_note, sensor_number):
+                    continue
+
+                # If the note is not the one we want to play, stop it
+                if active_note != note:
+                    stop_func, is_primed = active_notes.get(active_note, (None, False))
+
+                    if selected_instrument in BURST_INSTRUMENTS:
+                        active_notes[active_note] = (stop_func, False)
                     else:
-                        active_notes[active_note] = (None, False)
-            elif note == note_mappings.RESPONSE_IN_SPACING:
-                continue
-            else:
-                for tup in active_notes.copy().items():
-                    active_note = tup[0]
+                        stop_func()
+                        del active_notes[active_note]
 
-                    # We only want the notes this sensor is responsible for
-                    if not noteBelongsToSensor(active_note, sensor_number):
-                        continue
+            stop_func, is_primed = active_notes.get(note, (None, False))
 
-                    # If the note is not the one we want to play, stop it
-                    if active_note != note:
-                        stop_func, is_primed = active_notes.get(active_note, (None, False))
+            if note not in active_notes or (selected_instrument in BURST_INSTRUMENTS and not is_primed):
+                # if sensor_number == 2:
+                #     print("Playing note", note, distance, "cm")
+                active_notes[note] = (play(selected_instrument, note), True)
 
-                        if selected_instrument in BURST_INSTRUMENTS:
-                            active_notes[active_note] = (stop_func, False)
-                        else:
-                            stop_func()
-                            del active_notes[active_note]
-
-                stop_func, is_primed = active_notes.get(note, (None, False))
-
-                if note not in active_notes or (selected_instrument in BURST_INSTRUMENTS and not is_primed):
-                    # if sensor_number == 2:
-                    #     print("Playing note", note, distance, "cm")
-                    active_notes[note] = (play(selected_instrument, note), True)
-
-        sleep(1 / UPDATE_HZ)
-except KeyboardInterrupt:
-    print("Closing sensors")
-    close_sensors()
-    print("Done")
+    sleep(1 / UPDATE_HZ)
