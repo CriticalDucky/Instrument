7814680084b4bf409f17b2d821dfb721509a9d92
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-07-16 15:38:41 -0400

    Change to 1 sensor

--- scripts/tof.py
+++ scripts/tof.py
@@ -2,32 +2,36 @@ import VL53L0X  # type: ignore
 import RPi.GPIO as GPIO  # type: ignore
 from time import sleep
 
+# shutdown_pins = [(17, 0x29), (27, 0x2B), (22, 0x2D)]  # (pin, i2c_address)
+# tofs = [] # key = sensor number - 1, value = tof object
 
-shutdown_pins = [(17, 0x29), (27, 0x2B), (22, 0x2D)]  # (pin, i2c_address)
-tofs = [] # key = sensor number - 1, value = tof object
+# GPIO.setwarnings(False)
 
-GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BCM)
 
-GPIO.setmode(GPIO.BCM)
+# for pin in shutdown_pins:
+#     GPIO.setup(pin[0], GPIO.OUT)
+#     GPIO.output(pin[0], GPIO.LOW)
 
-for pin in shutdown_pins:
-    GPIO.setup(pin[0], GPIO.OUT)
-    GPIO.output(pin[0], GPIO.LOW)
+# # Keep all low for 500 ms or so to make sure they reset
+# sleep(0.50)
 
-# Keep all low for 500 ms or so to make sure they reset
-sleep(0.50)
+# for pin in shutdown_pins:
+#     tof = VL53L0X.VL53L0X(i2c_address=pin[1])
+#     tof.open()
+#     tofs += [tof]
+#     GPIO.output(pin[0], GPIO.HIGH)
 
-for pin in shutdown_pins:
-    tof = VL53L0X.VL53L0X(i2c_address=pin[1])
-    tof.open()
-    tofs += [tof]
-    GPIO.output(pin[0], GPIO.HIGH)
+# # Keep all high for 500 ms or so to make sure they turn on
+# sleep(0.50)
 
-# Keep all high for 500 ms or so to make sure they turn on
-sleep(0.50)
+tofs = [
+    VL53L0X.VL53L0X(i2c_address=0x29),
+]
 
 for tof in tofs:
-    tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+    tof.open()
+    tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.HIGH_SPEED)
 
 def get_distance(sensor_number):
     distance = tofs[sensor_number - 1].get_distance()/10
