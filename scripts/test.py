a03ff9de57b1a2f6eb3b56e81d226bae996d0388
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-08-02 23:33:45 -0400

    Add test

--- scripts/test.py
+++ scripts/test.py
@@ -19,25 +19,20 @@ Press Ctrl+C to exit.
 """
 Open and start the VL53L1X ranging sensor for each channel of the TCA9548A
 """
-tof1 = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29, tca9548a_num=1, tca9548a_addr=0x70)
-tof2 = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29, tca9548a_num=1, tca9548a_addr=0x71)
-tof3 = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29, tca9548a_num=2, tca9548a_addr=0x71)
+tof1 = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)#, tca9548a_num=1, tca9548a_addr=0x70)
+# tof2 = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29, tca9548a_num=1, tca9548a_addr=0x71)
 
 tof1.open()
 tof1.start_ranging(3)  # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
 
-tof2.open()
-tof2.start_ranging(3)  # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
-
-tof3.open()
-tof3.start_ranging(3)  # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
+# tof2.open()
+# tof2.start_ranging(3)  # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
 
 def exit_handler(signal, frame):
     global running
     running = False
     tof1.stop_ranging()
-    tof2.stop_ranging()
-    tof3.stop_ranging()
+    # tof2.stop_ranging()
     print()
     sys.exit(0)
 
@@ -49,10 +44,7 @@ while running:
     distance_in_mm = tof1.get_distance()
     print("Sensor 1 distance: {}mm".format(distance_in_mm))
 
-    distance_in_mm = tof2.get_distance()
-    print("Sensor 2 distance: {}mm".format(distance_in_mm))
-
-    distance_in_mm = tof3.get_distance()
-    print("Sensor 3 distance: {}mm".format(distance_in_mm))
+    # distance_in_mm = tof2.get_distance()
+    # print("Sensor 2 distance: {}mm".format(distance_in_mm))
     
     time.sleep(0.1)
