29ed91dc536f2f163b3028ca0db8f1872d0036d8
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-08-16 17:39:06 -0400

    test

--- scripts/tof.py
+++ scripts/tof.py
@@ -31,6 +31,7 @@ def get_distance(sensor_number):
         print(f"{device_name}X not found")
         return 0
 
+    print("ok noob", sensor_number - 1)
     distance_cm = cached_data_cm[sensor_number - 1]
 
     if distance_cm < 700 and distance_cm > 0:
