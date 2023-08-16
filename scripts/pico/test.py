ebe4b78e5753544d68e35a61c083b933a6196dd3
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-08-16 02:55:28 -0400

    Add test

--- scripts/pico/test.py
+++ scripts/pico/test.py
@@ -2,63 +2,101 @@ from machine import Pin, I2C #type: ignore
 from vl53l0x import setup_tofl_device, TBOOT
 import utime #type: ignore
 
-device_1_xshut = Pin(16, Pin.OUT)
-device_2_xshut = Pin(17, Pin.OUT)
-device_3_xshut = Pin(18, Pin.OUT)
-device_4_xshut = Pin(19, Pin.OUT)
-device_5_xshut = Pin(20, Pin.OUT)
-device_6_xshut = Pin(21, Pin.OUT)
+i2c_1_xshut = [
+    Pin(16, Pin.OUT),
+    Pin(17, Pin.OUT),
+    Pin(18, Pin.OUT),
+    Pin(19, Pin.OUT),
+    Pin(20, Pin.OUT),
+    Pin(21, Pin.OUT),
+]
+
+i2c_0_xshut = [
+    Pin(22, Pin.OUT),
+    Pin(26, Pin.OUT),
+    Pin(27, Pin.OUT),
+    Pin(28, Pin.OUT),
+    Pin(2, Pin.OUT),
+    Pin(3, Pin.OUT),
+]
 
 i2c_1 = I2C(id=1, sda=Pin(14), scl=Pin(15))
+i2c_0 = I2C(id=0, sda=Pin(0), scl=Pin(1))
 
-device_2_xshut.value(0)
-device_3_xshut.value(0)
-device_4_xshut.value(0)
-device_5_xshut.value(0)
-device_6_xshut.value(0)
+for pin in i2c_0_xshut + i2c_1_xshut:
+    pin.value(0)
 
-device_1_xshut.value(1)
+# i2c_1
+
+i2c_1_xshut[0].value(1)
 utime.sleep_us(TBOOT)
 tofl1 = setup_tofl_device(i2c_1, 40000, 12, 8)
 tofl1.set_address(0x31)
 
-device_2_xshut.value(1)
+i2c_1_xshut[1].value(1)
 utime.sleep_us(TBOOT)
 tofl2 = setup_tofl_device(i2c_1, 40000, 12, 8)
 tofl2.set_address(0x32)
 
-device_3_xshut.value(1)
+i2c_1_xshut[2].value(1)
 utime.sleep_us(TBOOT)
 tofl3 = setup_tofl_device(i2c_1, 40000, 12, 8)
 tofl3.set_address(0x33)
 
-device_4_xshut.value(1)
+i2c_1_xshut[3].value(1)
 utime.sleep_us(TBOOT)
 tofl4 = setup_tofl_device(i2c_1, 40000, 12, 8)
 tofl4.set_address(0x34)
 
-device_5_xshut.value(1)
+i2c_1_xshut[4].value(1)
 utime.sleep_us(TBOOT)
 tofl5 = setup_tofl_device(i2c_1, 40000, 12, 8)
 tofl5.set_address(0x35)
 
-device_6_xshut.value(1)
+i2c_1_xshut[5].value(1)
 utime.sleep_us(TBOOT)
 tofl6 = setup_tofl_device(i2c_1, 40000, 12, 8)
 tofl6.set_address(0x36)
 
+# i2c_0
+
+i2c_1_xshut[0].value(1)
+utime.sleep_us(TBOOT)
+tofl7 = setup_tofl_device(i2c_0, 40000, 12, 8)
+tofl7.set_address(0x31)
+
+i2c_0_xshut[1].value(1)
+utime.sleep_us(TBOOT)
+tofl8 = setup_tofl_device(i2c_0, 40000, 12, 8)
+tofl8.set_address(0x32)
+
+i2c_0_xshut[2].value(1)
+utime.sleep_us(TBOOT)
+tofl9 = setup_tofl_device(i2c_0, 40000, 12, 8)
+tofl9.set_address(0x33)
+
+i2c_0_xshut[3].value(1)
+utime.sleep_us(TBOOT)
+tofl10 = setup_tofl_device(i2c_0, 40000, 12, 8)
+tofl10.set_address(0x34)
+
+i2c_0_xshut[4].value(1)
+utime.sleep_us(TBOOT)
+tofl11 = setup_tofl_device(i2c_0, 40000, 12, 8)
+tofl11.set_address(0x35)
+
+i2c_0_xshut[5].value(1)
+utime.sleep_us(TBOOT)
+tofl12 = setup_tofl_device(i2c_0, 40000, 12, 8)
+tofl12.set_address(0x36)
+
 try:
     while True:
-        left, right, front, back, up, down = tofl1.ping(), tofl2.ping(), tofl3.ping(), tofl4.ping(), tofl5.ping(), tofl6.ping()
-        print(left, 'mm, ', right, 'mm, ', front, 'mm, ', back, 'mm, ', up, 'mm, ', down, 'mm')
+        print('   '.join([str(tofl.ping()/10) for tofl in [tofl1, tofl2, tofl3, tofl4, tofl5, tofl6, tofl7, tofl8, tofl9, tofl10, tofl11, tofl12]]))
 finally:
     # Restore default address
-    tofl1.set_address(0x29)
-    tofl2.set_address(0x29)
-    tofl3.set_address(0x29)
-    tofl4.set_address(0x29)
-    tofl5.set_address(0x29)
-    tofl6.set_address(0x29)
+    for tofl in [tofl1, tofl2, tofl3, tofl4, tofl5, tofl6, tofl7, tofl8, tofl9, tofl10, tofl11, tofl12]:
+        tofl.set_address(0x29)
 
 # from machine import Pin, I2C #type: ignore
 # from vl53l0x import setup_tofl_device, TBOOT
