130c9c67fcbb1e483b85322ac434212be91f8aff
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-11-23 09:39:52 -0500

    fix bug

--- scripts/window_test.py
+++ scripts/window_test.py
@@ -7,24 +7,38 @@ class FullScreenApp(App):
     def build(self):
         # Set the window to fullscreen
         Window.fullscreen = 'auto'
+
         # Create a layout for the new window
         layout = BoxLayout(orientation='horizontal')
-        
-        # Add ToggleButtons to the layout
+
+        # Create a vertical BoxLayout for the buttons
+        button_layout = BoxLayout(
+            orientation='vertical', size_hint=(None, 1), width=150)
+
+        # Add ToggleButtons to the vertical layout
         for i in range(5):
-            button = ToggleButton(text=f'Octave {i+2}', size_hint=(1, None), height=100, group='octaves', allow_no_selection=False)
+            button = ToggleButton(
+                text=f'Octave {i+2}',
+                group='octaves',
+                allow_no_selection=False
+            )
             button.bind(on_touch_down=self.on_touch_down)
-            
+
             # Set default state to enabled for Octave 4
             if i == 2:
                 button.state = 'down'
-            
-            layout.add_widget(button)
+
+            button_layout.add_widget(button)
+
+        # Add the vertical layout to the main horizontal layout
+        layout.add_widget(button_layout)
 
         return layout
 
     def on_touch_down(self, instance, touch):
-        print(f'Button {instance.text} pressed')
+        if instance.collide_point(*touch.pos):
+            print(f'Button {instance.text} pressed')
+
 
 if __name__ == '__main__':
     FullScreenApp().run()
