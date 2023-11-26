cd62d8bab6232d8338a2847c8cf8b0551dcec230
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-11-25 21:21:05 -0500

    add threading

--- scripts/program.py
+++ scripts/program.py
@@ -1,15 +1,18 @@
 import os
 import subprocess
+import threading
 
-# Set up the control panel
+def control_panel_thread():
+    current_script_path = os.path.abspath(__file__)
+    parent_dir = os.path.dirname(current_script_path)
+    os.chdir(parent_dir)
+    subprocess.run(['python3', 'control_panel.py'])
 
-current_script_path = os.path.abspath(__file__)
-parent_dir = os.path.dirname(current_script_path)
-os.chdir(parent_dir)
-subprocess.run(['python3', 'control_panel.py'])
+# Set up the control panel in a new thread
+control_panel_thread = threading.Thread(target=control_panel_thread)
+control_panel_thread.start()
 
 # Main loop
-
 from tof import get_sensor_binaries
 from note_controller import loop as note_controller_loop
 from led_controller import update_with_binaries
