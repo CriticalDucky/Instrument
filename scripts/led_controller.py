f8df3b7189ebe62bc36fc6c746521845166b5f0e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-02 19:22:45 -0500

    test

--- scripts/led_controller.py
+++ scripts/led_controller.py
@@ -262,7 +262,7 @@ def update_with_active_note_info(active_note_info: dict):
         # convert the hsv_colors list to a tuple
         average_color = average_hsv(tuple(tuple(row) for row in hsv_colors))
 
-        print("This is whats going in", (int(i) for i in colorsys.hsv_to_rgb(*average_color)))
+        print("This is whats going in:", tuple(tuple(row) for row in hsv_colors))
 
         final_data.append((int(i) for i in colorsys.hsv_to_rgb(*average_color)))
 
