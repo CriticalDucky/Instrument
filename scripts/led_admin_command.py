467dfc1a530971476c9db682d5d5d31bfab4f910
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-02 20:17:11 -0500

    Change snuff location

--- scripts/led_admin_command.py
+++ scripts/led_admin_command.py
@@ -1,16 +1,12 @@
 import sys
 import json
-import led_admin
 import time
+start_time = time.time()
+import led_admin
+print(f"Imported led_admin in {time.time() - start_time} seconds")
 
 json_string = sys.argv[1]
 
 data = json.loads(json_string)
 
-begin_time = time.time()
-
 led_admin.give_data(data)
-
-end_time = time.time()
-
-print(f'LED update took {end_time - begin_time} seconds')
