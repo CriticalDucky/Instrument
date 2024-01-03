20e692071a2fc793da8385a0648ee74838d0bef3
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-01-02 20:09:01 -0500

    Im stupid

--- scripts/led_admin_command.py
+++ scripts/led_admin_command.py
@@ -5,9 +5,5 @@ import led_admin
 json_string = sys.argv[1]
 
 data = json.loads(json_string)
-\
 
-for idx, val in enumerate(data):
-    strip.setPixelColor(idx, Color(val[0], val[1], val[2]))
-
-strip.show()
+led_admin.give_data(data)
