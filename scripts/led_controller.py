da1290220db175458bc815b6ec3a8d03bdd831c0
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-02-19 21:18:30 -0500

    test

--- scripts/led_controller.py
+++ scripts/led_controller.py
@@ -12,7 +12,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 
-client_socket.connect(('localhost', 50001))
+client_socket.connect((host, 50001))
 
 # Processes can be added to this table, and every frame they will update.
 led_processes = []
