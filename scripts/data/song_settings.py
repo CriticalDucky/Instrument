cf153b8c486bdc903f12d7a23360443e7d4a440b
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2024-04-28 14:59:38 -0400

    test

--- scripts/data/song_settings.py
+++ scripts/data/song_settings.py
@@ -1,8 +1,8 @@
-# BPM of the song "What I'm Becoming" by Cage the Elephant
-WIB_BPM = 135
-WIB_BPS = WIB_BPM / 60
-WIB_SPB = 1 / WIB_BPS
-WIB_SPB_DT = WIB_SPB / 2
+# # BPM of the song "What I'm Becoming" by Cage the Elephant
+# WIB_BPM = 135
+# WIB_BPS = WIB_BPM / 60
+# WIB_SPB = 1 / WIB_BPS
+# WIB_SPB_DT = WIB_SPB / 2
 
 # BPM of the song "Skin and Bones" by Cage the Elephant
 SAB_BPM = 160
--- scripts/song_player.py
+++ scripts/song_player.py
@@ -15,6 +15,7 @@ def start():
     pygame.init()
     pygame.mixer.init()
     pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play()
     start_time = time.time()  # Track the start time
     playing = True
