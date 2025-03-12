5d225308b8a2abf719f9acfa4bc23779258f2199
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-03-11 21:54:41 -0400

    Add LED Scheduler

--- scripts/led_scheduler.py
+++ scripts/led_scheduler.py
@@ -1,29 +1,28 @@
 import time
-from LEDProcess import LEDProcess
-
-'''
-This function 
-'''
+from LEDProcess import *
+from data.song_settings import *
 
 class LEDScheduler:
     def __init__(self):
         self.led_processes = []
 
-    def add_process(self, led_process: LEDProcess, start_time=None):
-        self.led_processes.append((led_process, start_time))
+    def add_process(self, led_process: LEDProcess):
+        self.led_processes.append(led_process)
 
-    def update_processes(self, progress_time):
-        for led_process, start_time in self.led_processes:
-            if start_time is None:
-                led_process.update(progress_time)
-            elif progress_time >= start_time:
-                led_process.update(progress_time - start_time)
+    def update_processes(self):
+        for led_process, _ in self.led_processes:
+            led_process.update()
 
-    def ping(self, progress_time=None):
-        if progress_time is None:
-            progress_time = time.time()
-        results = []
-        self.update_processes(progress_time)
+    def report(self):
+        results = [] # list of dicts: [{pixel_num: (r, g, b)}]
+        self.update_processes()
         for led_process, _ in self.led_processes:
             results.append(led_process.report())
         return results
+
+def begin():
+    start_time = time.time()
+
+    led_scheduler = LEDScheduler()
+
+    return led_scheduler
