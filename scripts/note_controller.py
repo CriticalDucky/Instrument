9e0787f26313e15b543636de18fab5cb97fc3637
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-03-11 22:49:20 -0400

    Add exception for song-playing

--- scripts/note_controller.py
+++ scripts/note_controller.py
@@ -2,6 +2,7 @@ from note_player import play
 from notation import *
 from tof import get_sensor_binaries
 import colorsys
+from song_player import is_playing
 
 # one table for each note in the octave (C-B)
 active_sensor_info = {i: [] for i in range(1, 13)}
@@ -93,9 +94,14 @@ def loop():
             library = get_selected_library()
             instrument = get_selected_instrument()
 
+            song_playing = is_playing()
+
             note = sensor_to_note(sensor_number) + str(octave)
 
-            if chord_type != 'None':
+            if song_playing:
+                notes = get_htpf_chord(note, inversion)
+                instance = ChordInstance(library, instrument, notes, note)
+            elif chord_type != 'None':
                 notes = create_chord(note, chord_type, inversion)
                 instance = ChordInstance(library, instrument, notes, note)
             else:
