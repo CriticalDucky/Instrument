4656aa1dc03f030ff4a64add9053aea80c804abb
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2025-03-13 00:12:59 -0400

    test

--- scripts/data/song_settings.py
+++ scripts/data/song_settings.py
@@ -11,7 +11,7 @@
 # SAB_SPB_DT = SAB_SPB / 2
 
 # BPM of the song 'Here to perform the final song' by Joywave
-HTPF_BPM = 71
+HTPF_BPM = 72
 HTPF_BPS = HTPF_BPM / 60
 HTPF_SPB = 1 / HTPF_BPS
 HTPF_SPB_DT = HTPF_SPB / 2
